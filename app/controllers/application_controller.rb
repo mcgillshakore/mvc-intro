390c87e85e9157c1e890494f5f5140bd9d857307
mcgillshakore <mcgills2093@gmail.com>
2020-06-23 17:45:38 -0500

    message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,20 @@ class ApplicationController < Sinatra::Base
 
     set(:views, 'app/views')
     
-
+    get('/list_airlines')do 
+        @airlines = Airline.all
+        erb :list_airlines
+    end 
+    get('/get-airline/:id') do
+        # binding.pry
+        @airline = Airline.find(params[:id])
+        @tickets = Ticket.all
+        erb :single_airline
+    end
+    delete('/remove-airline/:id') do
+        @airline = Airline.find(params[:id])
+        @airline.destroy()
+    end
 end
 
 
